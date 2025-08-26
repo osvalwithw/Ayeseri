e9fbd8669aa77b8af5ccc7d34d180a07ae567236
osvalwithw <124110579+osvalwithw@users.noreply.github.com>
2025-08-26 23:05:09 +0000

    082625-time update

--- MainPage/Overview/Err_functions.js
+++ MainPage/Overview/Err_functions.js
@@ -2,7 +2,8 @@ function display_errors() {
     const params = new URLSearchParams(window.location.search);
     const id = params.get('ID');
     const it = params.get('IT');
-    const timepar = params.get('timepar');
+    const Timeselect = params.get('Timepar');
+    console.log(`${id}, ${it}, ${Timeselect}`)
 
     const input = document.getElementById('EE_Number');
     if (input && id) {
@@ -10,13 +11,14 @@ function display_errors() {
     }
     document.getElementById('Header_1').textContent = `Overview Infotype ${it} errors`;
 
-    fetch(`https://ayeseri.onrender.com/employee_errors/${id}/${timepar}`)
+    fetch(`https://ayeseri.onrender.com/employee_errors/${id}/${Timeselect}`)
     .then(res => {
     if (!res.ok) throw new Error('Fallo la API');
         return res.json();
     })
     .then(data => {
-    mostrarTabla(data);
+    console.log(data.rows);
+    mostrarTabla(data.rows);
     })
     .catch(err => {
     console.error("❌ Error al obtener errores:", err);
@@ -27,6 +29,7 @@ function mostrarTabla(errores) {
     const params = new URLSearchParams(window.location.search);
     const id = params.get('ID');
     const it = params.get('IT');
+    const Timeselect = params.get('Timepar');
     const tbody = document.getElementById('tablaErrores');
     if (!tbody) {
         console.error("❌ No se encontró el tbody con id 'tablaErrores'");
