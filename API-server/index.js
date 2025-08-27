df4d87d2335fef8504ca381368ade2165d74a81a
osvalwithw <124110579+osvalwithw@users.noreply.github.com>
2025-08-27 23:05:35 +0000

    changes on upload requests

--- API-server/index.js
+++ API-server/index.js
@@ -117,15 +117,16 @@ app.get('/Requests/:NoTicket/:Username/:Email/:PSS', async (req, res) => {
   const Username = req.params.Username;
   const Email = req.params.Email;
   const PSS = req.params.PSS;
+  const params = [NoTicket, Username, Email, PSS];
 //  const encrypt = await bcrypt.hash(PSS, 10);
   console.log(`${NoTicket}, ${Username}, ${Email}, ${PSS}`);
   sql = `
   INSERT INTO Requests (NoTicket, User, Email, Psswd)
   VALUES (?, ?, ?, ?)`;
   try{
-    const [rows] = await pool.query(sql);
+    const [rows] = await pool.query(sql, params);
   } catch (e){
-    console.error('/Users:', e.message);
+    console.error('/Requests:', e.message);
     res.status(500).json({error: e.message});
   }
 });
